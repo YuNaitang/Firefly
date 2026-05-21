e52656b972acc49dc1e60370814f937775a7bab2
YuNaitang <3493995759@qq.com>
2026-05-21 10:41:18 +0800

    个性化，以及博客

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -75,7 +75,7 @@ export const siteConfig: SiteConfig = {
 		// 4. 网络图片: { type: "url", value: "https://example.com/logo.png", alt: "Logo" }
 		logo: {
 			type: "image",
-			value: "assets/images/yunaitang.png",
+			value: "assets/images/avatar.png",
 			alt: "🍀",
 		},
 		// 导航栏标题
